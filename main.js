f1e6cc426929f08d5e3ba1ebff844fbe143051e5
dritodev <pedroalejandrorosaperez@gmail.com>
2024-08-27 12:39:14 -0400

    agregando API_KEY

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3";
+const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3&&api_key=live_ZBuZrwg3MmKeg2ut46LciVMyiGu022BQJ0CXAy2A9U1jDEGScPUSoZu7WqjLAoz3";
 
 
 
